88b5ab664db0627e267d381deddc3639be4038ce
Daniel <167367785+u-iDaniel@users.noreply.github.com>
2024-05-31 15:15:31 -0600

    Add operation functions

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("test");
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        return "Error!";
+    }
+    return a / b;
+}
+
